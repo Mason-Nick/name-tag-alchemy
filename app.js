61820c3c2377867d0b0bd675cc2e24b26451e17b
Nick Mason <nickmasonswe@gmail.com>
2022-12-10 19:36:19 -0500

    Added js functionality.. bug on keyup func

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-// grab DOM elements
+const nameDisplay = document.getElementById('nameDisplay');
+const button = document.getElementById('updateButton');
+const nameInput = document.getElementById('nameInput');
+nameDisplay.textContent = nameInput;
+const colorChange = document.getElementById("nametag");
+//colorChange.style.backgroundColor= 'steelblue';
 
-// set event listeners
+
+button.addEventListener('click', ()=>{
+    if(colorChange.style.backgroundColor === 'red') {
+        colorChange.style.backgroundColor = 'steelblue';
+    } else {
+        colorChange.style.backgroundColor = 'red'
+    }
+    console.log('clicking away!');
+    return 'color changed';
+})
+    // nameDisplay.addEventListener('mouseover', ()=>{
+    //     if(nameDisplay.value === 'Nick'){
+    //         nameDisplay.textContent = `Whoa! it's magic!`
+    //     } else if(nameDisplay === `Whoa! it's magic!`) {
+    //         nameDisplay = 'Nick C Mason'
+    //     }
+        
+    // })
+    nameDisplay.addEventListener('keyup',()=>{
+        nameDisplay = nameInput.value;
+    })
